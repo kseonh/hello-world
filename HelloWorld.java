2fc5645d7e8b98b856c895aedaa8228b82803bd6
KimSeonHo <kimseonho.tech@gmail.com>
2021-03-29 13:26:12 +0900

    add jenkins print

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,7 @@
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
-		System.out.println("For Java");
+		System.out.println("Commit from Eclipse");
+		System.out.println("Check jenkins working");
 	}
 }
